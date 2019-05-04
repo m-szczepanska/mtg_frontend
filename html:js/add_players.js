3371ec668721434d21218a401fba03d6c0206ea8
Maria Szczepańska <szczepanskamaria93@gmail.com>
2019-05-04 23:18:16 +0200

    Add two new endpoints: 	- creating new tournament 	- adding players (existing already in db) to tournament

---
+++ html:js/add_players.js
@@ -0,0 +1,43 @@
+Vue.component('v-select', VueSelect.VueSelect);
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    errored: false,
+    correct: false,
+    players: null,
+    tournaments: null,
+    tournament: null,
+    player_ids: null
+  },
+  mounted () {
+      player_id = localStorage.getItem("player_id")
+      console.log(`http://localhost:8000/events/players/${player_id}/`)
+      axios.all([
+        axios.get('http://localhost:8000/events/players/',
+            {headers:
+              {'Authorization': (
+                String(localStorage.getItem("player_id")) + ':' + localStorage.getItem("token"))}
+            }),
+        axios.get('http://localhost:8000/events/tournaments/')]
+      ).then(axios.spread(
+          (playersRes, toursRes) => {(this.players = playersRes.data), (this.tournaments = toursRes.data), (console.log(this.players, this.tournaments))}
+      )).catch(error => {console.log(error)})
+  },
+  methods:{
+    selectForm: function (e) {
+        tournament = this.tournament
+        axios.post(
+            `http://localhost:8000/events/tournaments/${tournament}/add_players/`,
+            { player_ids: this.player_ids }
+      ).catch(error => {
+            console.log(error.response)
+      }).then(response => {
+          if (this.errored === false) {
+          console.log(this.correct)
+          this.correct = true}
+      })
+      e.preventDefault();
+    }
+  }
+})
---
+++ html:js/create_tournament.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+  el: '#app',
+  data: {
+    errored: false,
+    correct: false,
+    tour_name: null,
+    date_beginning: '2000-01-01T23:59',
+    date_ending: '2000-01-02T00:00'
+  },
+  methods:{
+    checkForm: function (e) {
+        axios.post(
+            'http://localhost:8000/events/tournaments/',
+            {
+                name: this.tour_name,
+                date_beginning: this.date_beginning,
+                date_ending: this.date_ending
+            },
+            {  headers: {
+              'Authorization': String(localStorage.getItem("player_id")) + ':' + localStorage.getItem("token")
+            }
+          }
+      ).catch(error => {if (!(error.response.data['error'])) {
+          this.errored = error.response.data['non_field_errors']
+      } else {this.errored = error.response.data['error']}
+      console.log(this.error.response.data)
+      }).then(response => {
+          if (this.errored === false) {
+          console.log(this.correct)
+          this.correct = true}
+      })
+      e.preventDefault();
+    }
+  }
+})
